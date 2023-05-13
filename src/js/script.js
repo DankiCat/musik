862065d4583fd57ef5f945be9515569eaf49732b
Cat <daniliukdaniil@gmail.com>
2023-05-13 19:51:05 +0300

    adding categories screen

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    let scrollTop = document.documentElement.scrollTop;
+
+    if (scrollTop > 100) {
+        header.classList.add('header_blue');
+    } else {
+        header.classList.remove('header_blue');
+    }
+});
+
+$(document).ready(function(){
+    $('.categories__carousel').slick({
+        arrows: false,
+        variableWidth: true,
+        centerMode: true,
+        initialSlide: 2,
+        focusOnSelect: true,
+        touchThreshold: 12,
+    });
+});
