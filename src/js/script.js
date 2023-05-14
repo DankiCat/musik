2a235a9eaef1f9475c39b9bbe0132f741833e7eb
Cat <daniliukdaniil@gmail.com>
2023-05-14 23:28:30 +0300

    addition schedule section

--- src/js/script.js
+++ src/js/script.js
@@ -23,3 +23,19 @@ $(document).ready(function(){
         touchThreshold: 12,
     });
 });
+
+/* Настройка табов в секции расписания */
+const tabs = document.querySelectorAll('.schedule__tab');
+const tabContent = document.querySelectorAll('.schedule__table .schedule__table-content');
+
+tabs.forEach((tab, index) => {
+    tab.addEventListener('click', (event) => {
+      event.preventDefault();
+  
+      tabs.forEach((tab) => tab.classList.remove('schedule__tab_active'));
+      tabContent.forEach((tab) => tab.classList.remove('schedule__table-content_active'));
+  
+      tab.classList.add('schedule__tab_active');
+      tabContent[index].classList.add('schedule__table-content_active');
+    });
+});
